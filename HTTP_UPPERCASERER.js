d2c285290ec568c131fba643939aaa7ec035b87f
Angad Gill <angadpalsingh92@gmail.com>
2016-08-31 18:23:18 +0530

    HTTP UPPERCASERER

---
+++ HTTP_UPPERCASERER.js
@@ -0,0 +1,12 @@
+var http = require('http')
+var map = require('through2-map')
+var port = Number(process.argv[2])
+
+var server = http.createServer(function (request, response) {
+  if (request.method === 'POST') {
+    request.pipe(map(function (chunk) {
+      return chunk.toString().toUpperCase()
+    })).pipe(response)
+  } return response.end('Only POST method Accepted')})
+
+server.listen(port)
