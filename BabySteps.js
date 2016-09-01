585e3bc3a2a338b37063ee5a4c3e414243d16760
Angad Gill <angadpalsingh92@gmail.com>
2016-09-01 10:00:55 +0530

    Shortened solution

--- BabySteps.js
+++ BabySteps.js
@@ -1,9 +1,3 @@
-var array = process.argv.slice(2) // stores the arguments after the node and path arguments
+var array = process.argv.slice(2).map(Number).reduce((a,b)=> a+b) // stores the arguments after the node and path arguments
 
-console.log(array
-  .map(function (a) { // converts every element of 'array' to a number
-    return Number(a)
-  })
-  .reduce(function (a, b) { // returns the sum of all the elements
-    return a + b
-  }, 0))
+console.log(array)
